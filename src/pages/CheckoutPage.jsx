f8202db9c36bcd5ece4dab4024d37881d31b2076
Kine Ones <kineones@Kines-MacBook-Air.local>
2025-08-11 20:47:59 +0200

    Fill inn temporary code to recuired pages

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/CheckoutPage.jsx
+
+export default function CheckoutPage() {
+    return (
+      <>
+        <h1>Checkout</h1>
+        <p>Her kommer utsjekk-skjema og betalingsinformasjon.</p>
+      </>
+    );
+  }
+  
