98ca03e21d6d7d9393a4d3ac506508555af894e8
Kine Ones <kineones@Kines-MacBook-Air.local>
2025-08-26 21:56:04 +0200

    Wrap App with CartProvidor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+import { CartProvider } from "./context/CartContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </BrowserRouter>
 );
